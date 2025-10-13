9ce999c0a57b0201d9f53b70325fc874b1adc0c6
theertha-ashokan <theerthauk13@gmail.com>
2025-10-13 22:53:26 +0530

    register completed,login logic

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,15 @@
 const users = require('../models/userModel')
+
+// register
 exports.registerController = async (req,res)=>{
     console.log("Inside Register API");
     // console.log(req.body);
     const {username,email,password} = req.body
-    console.log(username,email,password);
+    // console.log(username,email,password);
     try{
         const existingUser = await users.findOne({email})
+        console.log(existingUser);
+        
         if(existingUser){
             res.status(409).json("User already exist!!! Please Login")
         }else{
@@ -14,12 +18,38 @@ exports.registerController = async (req,res)=>{
                 email,
                 password
             })
+            
             await newUser.save()
+                        console.log(newUser);
+
             res.status(200).json(newUser)
         }
 
     }catch{
           res.status(500).json(err)
     }
+    
+}
+
+
+// login
+exports.loginController = async (req,res)=>{
+    console.log("Inside Login Api");
+
+    const {username,email,password} = req.body
+    console.log(username,email,password);
+    try{
+        const existingUser = await users.findOne({email,password})
+        if(existingUser){
+            res.status(200).json({user:existingUser})
+        }else{
+           
+            res.status(404).json("Invalid Email/Password")
+        }
+
+    }catch{
+          res.status(500).json(err)
+    }
+    
     
 }
--- routing/router.js
+++ routing/router.js
@@ -4,5 +4,8 @@ const router = express.Router()
 
 // register 
 router.post('/register',userController.registerController)
+// login
+router.post('/login',userController.loginController)
+
 
 module.exports = router
