d14ed7680ecfdca758e16e72759bdf04ff4d295f
theertha-ashokan <theerthauk13@gmail.com>
2025-10-14 21:27:57 +0530

    usercontroller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 const users = require('../models/userModel')
+const jwt =require('jsonwebtoken')
 
 // register
 exports.registerController = async (req,res)=>{
@@ -36,15 +37,22 @@ exports.registerController = async (req,res)=>{
 exports.loginController = async (req,res)=>{
     console.log("Inside Login Api");
 
-    const {username,email,password} = req.body
-    console.log(username,email,password);
+    const {email,password} = req.body
+    console.log(email,password);
     try{
-        const existingUser = await users.findOne({email,password})
+        const existingUser = await users.findOne({email})
         if(existingUser){
+            if(existingUser.password == password){
+                // token
+                const token =jwt.sign({userMail:existingUser.email},process.env.JWTSECRET)
+                res.status(200).json({user:existingUser,token})
+            }else{
+                res.status(200).json("invalid email /password...")
+            }
             res.status(200).json({user:existingUser})
         }else{
            
-            res.status(404).json("Invalid Email/Password")
+            res.status(404).json("Account doesnot exist...")
         }
 
     }catch{
