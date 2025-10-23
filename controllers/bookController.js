f5ce3d76e94e02cacfc547e85e33b7a6c4cf675a
theertha-ashokan <theerthauk13@gmail.com>
2025-10-23 19:51:53 +0530

    get user books,get user bought books,delete user books

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -28,7 +28,6 @@ exports.addBookController = async (req,res)=>{
 }
 
 // get home books
-
 exports.getHomeBooks = async(req,res) =>{
     console.log("Inside getHomeBooks");
     try{
@@ -63,6 +62,7 @@ exports.getAllBooks = async(req,res) =>{
     
 }
 
+// view books
 exports.viewBookController = async (req, res) => {
     console.log("Inside ViewBookController");
     const {id} = req.params
@@ -72,5 +72,51 @@ exports.viewBookController = async (req, res) => {
         res.status(200).json(viewBook)
     }catch(err){
         res.status(500).json(err)
+    }
+}
+
+
+// get all user books
+exports.getAllUserBooksController = async(req,res) =>{
+    console.log("Inside getAllUserBooksController");
+    const email = req.payload
+    try{
+
+        const allUserBooks = await books.find({userMail:email})
+        res.status(200).json(allUserBooks)
+
+    }catch(err){
+        res.status(500).json(err)
+    }
+    
+}
+
+
+// get all user bought book
+exports.getAllUserBoughtBooksController = async(req,res) =>{
+    console.log("Inside getAllUserBoughtBooksController");
+    const email = req.payload
+    try{
+
+        const allUserBoughtBooks = await books.find({bought:email})
+        res.status(200).json(allUserBoughtBooks)
+
+    }catch(err){
+        res.status(500).json(err)
+    }
+    
+}
+
+// removing user upload books
+exports.deleteUserBookController = async (req,res)=>{
+    console.log("Inside deleteUserBookController ");
+    // get book id
+    const {id} = req.params
+    console.log(id);
+    try{
+        await books.findByIdAndDelete({_id:id})
+        res.status(200).json("Deleted Successfully")
+    }catch(err){
+         res.status(500).json(err)
     }
 }
--- routing/router.js
+++ routing/router.js
@@ -8,16 +8,31 @@ const router = express.Router()
 // register 
 router.post('/register',userController.registerController)
 
+// login
 router.post('/login',userController.loginController)
 
+// google login
 router.post('/google-login',userController.googleLoginController)
 
+// add books
 router.post('/add-book',jwtMiddleware,multerConfig.array('uploadImages',3),bookController.addBookController)
 
+// home books
 router.get('/home-books',bookController.getHomeBooks)
 
+// all books
 router.get('/all-books',jwtMiddleware,bookController.getAllBooks)
 
+// view-books
 router.get('/books/:id/view',jwtMiddleware,bookController.viewBookController)
+
+// get user books
+router.get('/user-books',jwtMiddleware,bookController.getAllUserBooksController)
+
+// get user bought books
+router.get('/user-bought-books',jwtMiddleware,bookController.getAllUserBoughtBooksController)
+
+// delete user books
+router.get('/user-books/:id/remove',jwtMiddleware,bookController.deleteUserBookController)
 
 module.exports = router
